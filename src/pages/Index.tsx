c55373fca960b5a3b841712f45912c0a7a92cc1a
OmarDerwy <OmarDerwy@gmail.com>
2025-03-31 21:09:17 +0200

    feat(userAuth): if user isn't logged in then redirect to login screen

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,22 @@
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '@/components/layout/Layout';
 import { useUser } from '@/context/UserContext';
 import AdminDashboard from './Dashboard/AdminDashboard';
 import SupervisorDashboard from './Dashboard/SupervisorDashboard';
 import StudentDashboard from './Dashboard/StudentDashboard';
+import { useNavigate } from 'react-router-dom';
 
 const Index = () => {
+  const navigate = useNavigate();
   const { userRole } = useUser();
+
+  useEffect(() => {
+    if (!userRole) {
+      navigate('/login');
+    }
+  }, [userRole]);
+
+  if (!userRole) return null; // Block UI rendering if userRole is null
 
   return (
     <Layout>
