2b5e48ccb785cf1cc4bb83419e751c5c8cba347e
OmarDerwy <OmarDerwy@gmail.com>
2025-05-08 22:43:45 +0300

    FIX(introducedbyhuessein): remove strict mode 🤦

--- src/main.jsx
+++ src/main.jsx
@@ -11,17 +11,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Imp
 const queryClient = new QueryClient(); // Create a client instance
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}> {/* Wrap everything with QueryClientProvider */}
-			<BrowserRouter>
-				<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-					<UserProvider>
-						<PermissionsProvider>
-							<App />
-						</PermissionsProvider>
-					</UserProvider>
-				</ThemeProvider>
-			</BrowserRouter>
-		</QueryClientProvider>
-	</React.StrictMode>
+	<QueryClientProvider client={queryClient}> {/* Wrap everything with QueryClientProvider */}
+		<BrowserRouter>
+			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+				<UserProvider>
+					<PermissionsProvider>
+						<App />
+					</PermissionsProvider>
+				</UserProvider>
+			</ThemeProvider>
+		</BrowserRouter>
+	</QueryClientProvider>
 );
